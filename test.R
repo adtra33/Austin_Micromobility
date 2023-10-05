76740088550f8b070a93e5d5428bf8e7d72ec14f
Vedanth Vuthandam <126732661+vedvuth@users.noreply.github.com>
2023-10-05 17:40:27 -0500

    test.R

--- test.R
+++ test.R
@@ -7,7 +7,6 @@ table(readf$Month)
 y2020 <-  readf %>% mutate(Month2= recode(Month, 'Jan','Feb','Mar','Apr',
                                          'May','Jun','Jul','Aug','Sep','Oct',
                                          'Nov','Dec'))
-covid1 <- read.csv('Trips2019[1-6].csv')
 
 ggplot(data = y2020, aes(x = Month2, y = Trip.Distance)) + 
   geom_bar(aes(y = Trip.Distance), stat = 'summary', fun = mean)
